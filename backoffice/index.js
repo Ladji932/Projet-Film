d18f6afbeabfa55a8cfcbeee0cebe3e1fa617f3d
Ladji27 <maigaladji47@gmail.com>
2024-05-23 20:38:18 +0200

    modification

--- backoffice/index.js
+++ backoffice/index.js
@@ -1,29 +1,27 @@
 const express = require('express');
 const app = express();
 const routes = require('./Routes/router');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 app.set('view engine', 'ejs');
 
 dotenv.config();
 
+// Connexion à MongoDB avec les options mises à jour
 //mongoose.connect('mongodb://localhost:27017/filmsDB');
-mongoose.connect('mongodb+srv://maigaladji47:Lmaiga28032002.@cluster0.dsdxrbb.mongodb.net/filmsDB', { 
-  useNewUrlParser: true, 
-  useUnifiedTopology: true,
-  serverSelectionTimeoutMS: 30000 // Augmente le délai de sélection du serveur
+mongoose.connect(process.env.MONGODB_URI, { 
+  serverSelectionTimeoutMS: 30000, // Délai de sélection du serveur (30 secondes)
+  connectTimeoutMS: 30000, // 30 secondes pour la connexion
+  socketTimeoutMS: 45000 // 45 secondes pour les opérations de socket
 });
-//console.log(process.env.MONGODB_URI)
-//mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Erreur de connexion à MongoDB : '));
 db.once('open', function() {
   console.log("Connecté à MongoDB");
 });
 
-
 app.use('/', routes);
-
 
 const port = 4040;
 app.listen(port, () => {
