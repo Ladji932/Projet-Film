029852797f2692e48ab9b6132607caba589da4f1
Ladji27 <maigaladji47@gmail.com>
2024-05-23 20:55:20 +0200

    modification

--- backoffice/index.js
+++ backoffice/index.js
@@ -9,11 +9,12 @@ dotenv.config();
 
 // Connexion à MongoDB avec les options mises à jour
 //mongoose.connect('mongodb://localhost:27017/filmsDB');
-mongoose.connect(process.env.MONGODB_URI, { 
+mongoose.connect('mongodb+srv://maigaladji47:Lmaiga28032002.@cluster0.dsdxrbb.mongodb.net/filmsDB');
+/*mongoose.connect(process.env.MONGODB_URI, { 
   serverSelectionTimeoutMS: 30000, // Délai de sélection du serveur (30 secondes)
   connectTimeoutMS: 30000, // 30 secondes pour la connexion
   socketTimeoutMS: 45000 // 45 secondes pour les opérations de socket
-});
+});*/
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Erreur de connexion à MongoDB : '));
